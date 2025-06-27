b7a9ed73854a00d811773fb52f790f363ad5037b
aravindav <aravindwaitz@gmail.com>
2025-06-27 19:13:58 -0400

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -15,6 +15,6 @@ public class Application {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot - Testing 4";
+        return "Hello from Spring Boot - Testing 5";
     }
 }
